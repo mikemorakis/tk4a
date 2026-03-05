6046cf192731a9ea83aa9a20941dda1109f60e0c
Mike Morakis <michalismorakis@gmai.com>
2026-03-05 11:17:03 +0200

    Add sitemap.xml and robots.txt for SEO

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,6 +1,7 @@
 module.exports = {
   eleventyComputed: {
     permalink: (data) => {
+      if (data.permalink) return data.permalink;
       const slug = data.page.fileSlug;
       if (!slug || slug === "index") return "/index.html";
       return `/${slug}.html`;
